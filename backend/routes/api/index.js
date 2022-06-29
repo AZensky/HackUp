ea5a1112c6e25df1bc328217bd05f8b736a5ecef
alex <alexzelinsky124@gmail.com>
2022-06-29 12:21:57 -0700

    add user logout backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,19 +1,19 @@
 const router = require("express").Router();
-// const { setTokenCookie } = require("../../utils/auth.js");
-// const { User } = require("../../db/models");
+const { setTokenCookie } = require("../../utils/auth.js");
+const { User } = require("../../db/models");
 const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
 const { restoreUser } = require("../../utils/auth.js");
-// const { requireAuth } = require("../../utils/auth.js");
+const { requireAuth } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
 // If current user session is valid, set req.user to the user in the database
 // If current user session is not valid, set req.user to null
 router.use(restoreUser);
 
-// router.get("/require-auth", requireAuth, (req, res) => {
-//   return res.json(req.user);
-// });
+router.get("/require-auth", requireAuth, (req, res) => {
+  return res.json(req.user);
+});
 
 router.use("/session", sessionRouter);
 
@@ -23,19 +23,19 @@ router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
 });
 
-// router.get("/restore-user", (req, res) => {
-//   return res.json(req.user);
-// });
+router.get("/restore-user", (req, res) => {
+  return res.json(req.user);
+});
 
-// router.get("/set-token-cookie", async (_req, res) => {
-//   const user = await User.findOne({
-//     where: {
-//       username: "Demo-lition",
-//     },
-//   });
-//   setTokenCookie(res, user);
-//   return res.json({ user });
-// });
+router.get("/set-token-cookie", async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: "Demo-lition",
+    },
+  });
+  setTokenCookie(res, user);
+  return res.json({ user });
+});
 
 // Add a XSRF-TOKEN cookie
 router.get("/csrf/restore", (req, res) => {
