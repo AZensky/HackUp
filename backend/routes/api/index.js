e13b87e455850adab6b753ccd0e410b8b5744e25
alex <alexzelinsky124@gmail.com>
2022-06-29 13:24:21 -0700

    validate signup functionality

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -19,9 +19,9 @@ router.use("/session", sessionRouter);
 
 router.use("/users", usersRouter);
 
-router.post("/test", (req, res) => {
-  res.json({ requestBody: req.body });
-});
+// router.post("/test", (req, res) => {
+//   res.json({ requestBody: req.body });
+// });
 
 router.get("/restore-user", (req, res) => {
   return res.json(req.user);
