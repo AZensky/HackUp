48bb772c600fdd0bbca615dbcdc4295fbdee21bf
alex <alexzelinsky124@gmail.com>
2022-06-29 10:55:41 -0700

    user authentication express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,31 @@
 const router = require("express").Router();
+const { setTokenCookie } = require("../../utils/auth.js");
+const { User } = require("../../db/models");
+const { restoreUser } = require("../../utils/auth.js");
+// const { requireAuth } = require("../../utils/auth.js");
+
+// Connect restoreUser middleware to the API router
+// If current user session is valid, set req.user to the user in the database
+// If current user session is not valid, set req.user to null
+router.use(restoreUser);
+
+// router.get("/require-auth", requireAuth, (req, res) => {
+//   return res.json(req.user);
+// });
+
+router.get("/restore-user", (req, res) => {
+  return res.json(req.user);
+});
+
+router.get("/set-token-cookie", async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: "Demo-lition",
+    },
+  });
+  setTokenCookie(res, user);
+  return res.json({ user });
+});
 
 // Add a XSRF-TOKEN cookie
 router.get("/csrf/restore", (req, res) => {
