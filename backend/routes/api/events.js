ac78e32eca72adf95368661a4684ef8dda110261
alex <alexzelinsky124@gmail.com>
2022-07-01 19:37:22 -0700

    event details by id route handler

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -3,6 +3,26 @@ const { Event, Group } = require("../../db/models");
 
 const router = express.Router();
 
+router.get("/:eventId", async (req, res) => {
+  const event = await Event.findByPk(req.params.eventId, {
+    attributes: { exclude: ["previewImage", "createdAt", "updatedAt"] },
+    include: {
+      model: Group,
+      attributes: ["id", "name", "private", "city", "state"],
+    },
+  });
+
+  if (!event) {
+    res.status(404);
+    res.json({
+      message: "Event couldn't be found",
+      statusCode: 404,
+    });
+  }
+
+  res.json(event);
+});
+
 //route handler for getting all events, need to add venues association later
 router.get("/", async (req, res) => {
   const events = await Event.findAll({
