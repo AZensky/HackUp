1004430591b70bf639fc21d69cb7b633e61f4861
alex <alexzelinsky124@gmail.com>
2022-07-02 13:46:05 -0700

    created-group-members

--- backend/db/models/groupmember.js
+++ backend/db/models/groupmember.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      GroupMember.belongsTo(models.User, {
+        foreignKey: "userId",
+        as: "Membership",
+      });
     }
   }
   GroupMember.init(
