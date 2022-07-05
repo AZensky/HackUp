74a2ffca62d8bb8dbbfc9fae16818b55f9f6ae3a
alex <alexzelinsky124@gmail.com>
2022-07-05 13:15:52 -0700

    added Venue seeder

--- backend/db/migrations/20220702000827-create-venue.js
+++ backend/db/migrations/20220702000827-create-venue.js
@@ -29,12 +29,14 @@ module.exports = {
         type: Sequelize.DECIMAL,
       },
       createdAt: {
-        allowNull: false,
+        // allowNull: false,
         type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
       updatedAt: {
-        allowNull: false,
+        // allowNull: false,
         type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
     });
   },
