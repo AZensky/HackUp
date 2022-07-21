63c9570864ab80191ea1eec735dcf0f8b5ce4651
alex <alexzelinsky124@gmail.com>
2022-07-21 11:38:04 -0700

    session reducer

---
+++ frontend/src/store/session.js
@@ -0,0 +1,53 @@
+import { csrfFetch } from "./csrf";
+
+const SET_USER = "session/SET_USER";
+const REMOVE_USER = "session/REMOVE_USER";
+
+//action creator to set the session user
+export const setUser = (user) => {
+  return {
+    type: SET_USER,
+    payload: user,
+  };
+};
+
+//action creator to remove the session user
+export const removeUser = () => {
+  return {
+    type: REMOVE_USER,
+  };
+};
+
+export const thunkSetUser = (user) => async (dispatch) => {
+  const { email, password } = user;
+  const response = await csrfFetch("/api/session", {
+    method: "POST",
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return data;
+  }
+};
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USER: {
+      const newState = { ...state };
+      newState.user = action.payload;
+      return newState;
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export default sessionReducer;
