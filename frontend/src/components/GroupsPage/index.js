1a5ab729126b288acb8087d998e90c39ebf06286
alex <alexzelinsky124@gmail.com>
2022-07-22 11:26:55 -0700

    added dispatch and selector for groups page

--- frontend/src/components/GroupsPage/index.js
+++ frontend/src/components/GroupsPage/index.js
@@ -1,13 +1,22 @@
 import Reac, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getAllGroups } from "../../store/groups";
 
 function GroupsPage() {
   const dispatch = useDispatch();
+  const groups = Object.values(useSelector((state) => state.groups));
+  console.log(groups);
   useEffect(() => {
     dispatch(getAllGroups());
-  }, [dispatch]);
-  return <div>GroupsPage</div>;
+  }, []);
+
+  return (
+    <div>
+      {groups.map((group) => (
+        <span>{group.name}</span>
+      ))}
+    </div>
+  );
 }
 
 export default GroupsPage;
