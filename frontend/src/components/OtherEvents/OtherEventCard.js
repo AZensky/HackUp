4f2cd9b69abedcba4855944c77aafa724119921a
alex <alexzelinsky124@gmail.com>
2022-07-25 12:36:32 -0700

    skeleton of event-details-page complete

--- frontend/src/components/OtherEvents/OtherEventCard.js
+++ frontend/src/components/OtherEvents/OtherEventCard.js
@@ -4,7 +4,19 @@ import "./OtherEventCard.css";
 function OtherEventCard() {
   return (
     <div class="other-event-card-container">
-      <div className="other-event-card"></div>
+      <div className="other-event-card">
+        <div className="other-event-details">
+          <time>Fri, Mar 3 · 6:30 PM PDT</time>
+          <h4>Happy Hour </h4>
+          <p>20-30ish Midtown Women's Group </p>
+          <div className="other-events-location">
+            <i class="fa-solid fa-lg fa-location-pin"></i>
+            <span>1716 L Street · Sacramento, CA</span>
+          </div>
+        </div>
+
+        <div className="other-event-numAttendees">37 Attendees</div>
+      </div>
     </div>
   );
 }
