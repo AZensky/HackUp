74da48841050a97e9479c363c8f92d271d2450d2
alex <alexzelinsky124@gmail.com>
2022-07-25 15:53:33 -0700

    skeleton of group-details-added

---
+++ frontend/src/components/GroupDetailsPage/GroupUpcomingEvents.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./GroupUpcomingEvents.js";
+
+function GroupUpcomingEvents() {
+  return <div className="upcoming-event-container">GroupUpcomingEvents</div>;
+}
+
+export default GroupUpcomingEvents;
--- frontend/src/components/GroupDetailsPage/index.js
+++ frontend/src/components/GroupDetailsPage/index.js
@@ -1,13 +1,38 @@
 import React from "react";
 import "./GroupDetailsPage.css";
 import GroupDetailsHeader from "./GroupDetailsHeader";
+import GroupUpcomingEvents from "./GroupUpcomingEvents";
 
 function GroupDetailsPage() {
   return (
     <>
       <GroupDetailsHeader />
       <div className="group-details-page__main-content-container">
-        <div className="group-details-page__main-content"></div>
+        <div className="group-details-page__main-content">
+          <div className="about-and-upcoming-container">
+            <div className="group-details__about-section">
+              <h4>What we're about</h4>
+              <p>
+                The Monthly dev is a series of online events brought to you with
+                love by daily.dev. It's a place for softwar engineers to gath
+                and hear world-class The Monthly dev is a series of online
+                events brought to you with love by daily.dev. It's a place for
+                softwar engineers to gath and hear world-class The Monthly dev
+                is a series of online events brought to you with love by
+                daily.dev. It's a place for softwar engineers to gath and hear
+                world-class The Monthly dev is a series of online events brought
+                to you with love by daily.dev. It's a place for softwar
+                engineers to gath and hear world-class The Monthly dev is a
+                series of online events brought to you with love by daily.dev.
+                It's a place for softwar engineers to gath and hear world-class
+              </p>
+            </div>
+            <h4>Upcoming Events</h4>
+            <GroupUpcomingEvents />
+          </div>
+
+          <div className="organizer-and-members-container"></div>
+        </div>
       </div>
     </>
   );
