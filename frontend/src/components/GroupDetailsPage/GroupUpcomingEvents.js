50bd6782343e281a8f54f617ed6e08731dc52e27
alex <alexzelinsky124@gmail.com>
2022-07-27 15:34:25 -0700

    updated group details time

--- frontend/src/components/GroupDetailsPage/GroupUpcomingEvents.js
+++ frontend/src/components/GroupDetailsPage/GroupUpcomingEvents.js
@@ -16,16 +16,63 @@ function GroupUpcomingEvents() {
     getGroupEvents().catch(console.error);
   }, []);
 
+  let days = {
+    0: "Sunday",
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+  };
+
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let timeStrings = [];
+
+  if (upcomingEvents.length > 0) {
+    timeStrings = new Array(upcomingEvents.length).fill("");
+
+    upcomingEvents.forEach((event, idx) => {
+      let startDate = event.startDate;
+      const newDate = new Date(startDate);
+      const date = newDate.getUTCDate();
+      let day = days[newDate.getDay()];
+      let hours = newDate.getHours();
+      let month = monthNames[newDate.getMonth()];
+      const minutes = newDate.getMinutes();
+      let year = newDate.getFullYear();
+      timeStrings[idx] = `${day}, ${month} ${date}, ${year} · ${
+        hours > 12 ? hours - 12 : hours
+      }:${minutes.length > 1 ? minutes : "0" + minutes} ${
+        hours > 12 ? "PM" : "AM"
+      }`;
+    });
+  }
+
   return (
     <div className="group-upcoming-event-container">
       {upcomingEvents.length > 0 &&
-        upcomingEvents.map((event) => (
+        upcomingEvents.map((event, idx) => (
           <Link
             key={event.id}
             to={`/events/${event.id}`}
             className="group-upcoming-event-info-container"
           >
-            <time>Fri, Mar 3, 2023, 8:00 AM PST</time>
+            <time>{timeStrings[idx]}</time>
             <span>{event.name}</span>
             <div className="group-upcoming-event-attendees-container">
               <i className="fa-solid fa-user-large"></i>
